1a330c4150e79ce12e3111b8f2b4c7bb02a6d6bf
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2020-02-14 16:52:50 +0000

    pil asru amend test for autoresolved pil amendments

---
+++ test/data/tasks/pil/pil-amendment-asru.js
@@ -0,0 +1,76 @@
+module.exports = {
+  id: '884846c7-9901-462a-9dd0-b0001fad9368',
+  meta: {
+    previous: 'new',
+    next: 'autoresolved',
+    user: {
+      id: 'dee8605d-2113-4411-8a0a-dc131af87a76',
+      profile: {
+        id: 'a942ffc7-e7ca-4d76-a001-0b5048a057d2',
+        title: 'Miss',
+        firstName: 'Li Sen',
+        lastName: 'Xing',
+        email: 'asru-licensing@homeoffice.gov.uk',
+        asruUser: true,
+        asruAdmin: false,
+        asruLicensing: true,
+        asruInspector: false,
+        name: 'Li Sen Xing'
+      }
+    },
+    payload: {
+      data: {
+        procedures: ['B'],
+        notesCatD: 'Cat D notes',
+        notesCatF: 'Cat F notes',
+        species: ['Mice'],
+        establishmentId: 8201,
+        profileId: '18b00ea5-2fc7-47a5-91d1-aa7995159aa0'
+      },
+      meta: {},
+      model: 'pil',
+      id: '0c7ac573-f4c2-4f2e-b6a0-684d6d4a52d7',
+      action: 'grant',
+      changedBy: 'a942ffc7-e7ca-4d76-a001-0b5048a057d2'
+    }
+  },
+  event: 'status:new:autoresolved',
+  status: 'autoresolved',
+  data: {
+    id: '0c7ac573-f4c2-4f2e-b6a0-684d6d4a52d7',
+    data: {
+      species: ['Mice'],
+      notesCatD: 'Cat D notes',
+      notesCatF: 'Cat F notes',
+      profileId: '18b00ea5-2fc7-47a5-91d1-aa7995159aa0',
+      procedures: ['B'],
+      establishmentId: 8201
+    },
+    meta: {},
+    model: 'pil',
+    action: 'grant',
+    subject: '304235c0-1a83-49f0-87ca-b11b1ad1e147',
+    changedBy: 'a942ffc7-e7ca-4d76-a001-0b5048a057d2',
+    modelData: {
+      id: '0c7ac573-f4c2-4f2e-b6a0-684d6d4a52d7',
+      status: 'active',
+      deleted: null,
+      species: null,
+      createdAt: '2020-02-13T11:13:48.371Z',
+      issueDate: '2015-07-02T00:00:00.000Z',
+      notesCatD: 'Cat D notes',
+      notesCatF: 'Cat F notes',
+      profileId: '304235c0-1a83-49f0-87ca-b11b1ad1e147',
+      updatedAt: '2020-02-13T11:13:48.371Z',
+      conditions: null,
+      migratedId: null,
+      procedures: ['B', 'F', 'C', 'D'],
+      reviewDate: null,
+      licenceNumber: 'UT-542791',
+      revocationDate: null,
+      establishmentId: 8201
+    },
+    establishmentId: 8201,
+    initiatedByAsru: true
+  }
+};
