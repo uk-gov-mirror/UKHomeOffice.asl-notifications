b1f4ee991c669da6d96f89809361becae3a5a90c
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2022-07-01 17:31:42 +0100

    add reminders job

---
+++ jobs/reminder-notice.js
@@ -0,0 +1,52 @@
+const moment = require('moment');
+const Emailer = require('../lib/emailer');
+
+module.exports = async ({ schema, logger, publicUrl }) => {
+  const { Reminder } = schema;
+
+  const emailer = Emailer({ schema, logger, publicUrl });
+
+  const reminderNotice = async ({ value, unit }) => {
+    const when = value ? `in ${value} ${unit}` : unit;
+    const action = `condition-reminder-${value ? `${value}-${unit}` : unit}`;
+
+    logger.debug(`Looking for reminders due ${when}`);
+
+    let start = moment();
+    let end = moment();
+
+    switch (unit) {
+      case 'week':
+        start = moment().add(1, 'day');
+        end = moment().add(value, 'week');
+        break;
+
+      case 'month':
+        start = moment().add(1, 'week');
+        end = moment().add(value, 'month');
+        break;
+    }
+
+    const reminders = await Reminder.query()
+      .where('deadline', '>', start.startOf('day').toISOString())
+      .where('deadline', '<=', end.endOf('day').toISOString())
+      .where('status', 'active');
+
+    logger.debug(`Found ${reminders.length} reminders due`);
+
+    return Promise.all(reminders.map(reminder => emailer({
+      event: 'direct-notification',
+      data: {
+        id: reminder.modelId,
+        model: reminder.modelType,
+        when,
+        action,
+        reminder
+      }
+    })));
+  };
+
+  await reminderNotice({ value: 1, unit: 'month' });
+  await reminderNotice({ value: 1, unit: 'week' });
+  await reminderNotice({ unit: 'today' });
+};
